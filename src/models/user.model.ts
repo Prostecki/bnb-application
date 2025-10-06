4fdab4c0c1da390a7679c619ff3c33784b584062
Mark Taratynov <mark.taratynov@gmail.com>
2025-10-06 20:49:12 +0200

    Remove authentication and booking management features, including controllers, routes, services, models, and related configurations.

--- src/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  password: string; // For security, this should be a password hash
-  isAdmin: boolean;
-}
-
-export type UserCredentials = Omit<User, "id" | "isAdmin">;
